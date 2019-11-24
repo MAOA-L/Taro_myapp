691c4433358df0aff608755ba349afbae9cfba41
CyanZoy <1099312914@qq.com>
2019-11-24 22:13:44 +0800

    big_update

--- src/app.tsx
+++ src/app.tsx
@@ -56,7 +56,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this['host'] = 'http://127.0.0.1';
+        this['host'] = 'http://ws.cyanzoy.top';
     }
 
     componentDidShow() {
